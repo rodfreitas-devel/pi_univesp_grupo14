da077aedfd89fe66a24e417aa5f7003b096c65f3
Rodrigo C. de Freitas <rodcesar.freitas@protonmail.com>
2026-05-02 01:21:22 -0300

    atualização arquivo run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-podman run -d \
+podman run -d --replace \
   --name mysql-db \
   -e MYSQL_ROOT_PASSWORD=root123 \
   -e MYSQL_DATABASE=storage \
   -p 3306:3306 \
-  -v ./mysql_data:/var/lib/mysql \
+  -v "$(pwd)/mysql_data:/var/lib/mysql" \
   docker.io/library/mysql:8.0
 
 podman exec -it mysql-db bash
+#cd /var/lib/mysql
