ce8f57b3a07e9c26676812b18b1340f8bb6d988a
Rodrigo C. de Freitas <rodcesar.freitas@protonmail.com>
2026-05-02 17:28:24 -0300

    atualização arquivo server/user.py

--- server/user.py
+++ server/user.py
@@ -1,13 +1,16 @@
 from server.db import get_connection
 
 def check_login(username, password):
+    """
+    Retorna o usuário se encontrado na tabela 'usuario', caso contrário None
+    """
     conn = get_connection()
     try:
         with conn.cursor() as cursor:
-            sql = "SELECT * FROM users WHERE username=%s AND password=%s"
+            sql = "SELECT * FROM usuario WHERE userName=%s AND passwd=%s"
             cursor.execute(sql, (username, password))
-            result = cursor.fetchone()
-            return result  # retorna None se não encontrar
+            result = cursor.fetchone()  # retorna None se não encontrar
+            return result
     finally:
         conn.close()
 
