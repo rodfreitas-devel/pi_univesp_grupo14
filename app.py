2c2315adf83492d1b26a5cf582b2cabdb6c44ded
Rodrigo C. de Freitas <rodcesar.freitas@protonmail.com>
2026-05-02 17:34:41 -0300

    atualização arquivo app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
-from server.user import check_login
+from server.user import check_login  
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="client")  
 
+# raiz redireciona para login
 @app.route("/")
+@app.route("/index")
+def index():
+    return redirect(url_for("login"))
+
+# rota de login
 @app.route("/login", methods=["GET", "POST"])
 def login():
     error = None
@@ -12,13 +18,20 @@ def login():
         password = request.form.get("password")
         user = check_login(username, password)
 
-        ''' >>>> teste de conexão com banco de dados, isso pode ser excluído <<<<'''
         if user:
-            return f"Bem-vindo, {user['username']}!"
+#            return redirect(url_for("home"))  # login ok → redireciona para home
+             return "Login ok"   
         else:
-            error = "Usuário ou senha inválidos"
+            #error = "Usuário ou senha inválidos"
+            return "usuário ou senha inválidos"
 
     return render_template("login.html", error=error)
 
+# rota home após login
+@app.route("/home")
+def home():
+    return render_template("home.html")  # página principal após login
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
